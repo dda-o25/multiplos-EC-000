a41516252135c616bae81df4ff76041f2d2ada7a
EC-000 <eduardoc.castillo9814@gmail.com>
2025-09-18 21:07:16 -0600

    Revision 3
    Solucionar que el 0 si sea multiplo de los otros numeros excepto de si mismo

--- multiplos.py
+++ multiplos.py
@@ -1,19 +1,19 @@
 """
 Multiplos
-El propósito de este programa es calcular si un numero dado es multiplo de otro numero, y en caso contrario madnar un mensaje de error
+El propósito de este programa es calcular si un numero dado es multiplo de otro numero, y en caso contrario mandar un mensaje de error
 Eduardo Caleb Castillo Llanas 18/Sep/25
 """
 
 valor_1 = int(input())
 valor_2 = int(input())
 
-# Primero validamos ceros
-if valor_1 == 0 or valor_2 == 0:
-    print("No se puede verificar múltiplos con 0")
+if valor_1 != 0 and valor_2 % valor_1 == 0:
+    print("El número", valor_2, "es múltiplo del", valor_1)
+elif valor_2 != 0 and valor_1 % valor_2 == 0:
+    print("El número", valor_1, "es múltiplo del", valor_2)
+elif valor_1 == 0 and valor_2 != 0:
+    print("El número 0 es múltiplo del", valor_2)
+elif valor_2 == 0 and valor_1 != 0:
+    print("El número 0 es múltiplo del", valor_1)
 else:
-    if valor_2 % valor_1 == 0:
-        print("El número", valor_2, "es múltiplo del", valor_1)
-    elif valor_1 % valor_2 == 0:
-        print("El número", valor_1, "es múltiplo del", valor_2)
-    else:
-        print("Ninguno de los números es múltiplo del otro")
+    print("Ninguno de los números es múltiplo del otro")
