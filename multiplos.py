9868db8300ebe2ea1229f68bf7a9126b63b24e26
EC-000 <eduardoc.castillo9814@gmail.com>
2025-09-18 20:55:58 -0600

    Prueba V.2
    No pude subirlo desde mi pc asi que lo subi directo en el GitHub

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,18 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Multiplos
+El propósito de este programa es calcular si un numero dado es multiplo de otro numero, y en caso contrario madnar un mensaje de error
+Eduardo Caleb Castillo Llanas 18/Sep/25
 """
 
-# Declaraciones
-CONSTANTE = valor
+valor_1 = int(input("Primer número: "))
+valor_2 = int(input("Segundo número: "))
+prueba_1 = valor_2 % valor_1
+prueba_2 = valor_1 % valor_2
 
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+# Proces0
+if prueba_1 == 0:
+    print(valor_2, "es múltiplo de", valor_1)
+elif prueba_2 == 0:
+    print(valor_1, "es múltiplo de", valor_2)
+else:
+    print("Ninguno de los números es múltiplo del otro")
